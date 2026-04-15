4c1e12e1a851f5756fb3b965888a0c80ae3eae7d
dinog <dinogamer089@gmail.com>
2026-04-14 22:22:30 -0700

    Correcion del yml

--- BD_Script/CELS.sql
+++ BD_Script/CELS.sql
@@ -1,11 +1,11 @@
-INSERT INTO usuario (`id_usuario`,`codigo_tipo_usuario`,`nombre`,`apellido_prim`,`apellido_seg`,`fecha_nacimiento`,`fecha_creacion_usuario`,`estado`) VALUES (1,0,'John','Garcia','Viray','2005-01-09','2026-04-13 13:11:41',0);
-INSERT INTO usuario (`id_usuario`,`codigo_tipo_usuario`,`nombre`,`apellido_prim`,`apellido_seg`,`fecha_nacimiento`,`fecha_creacion_usuario`,`estado`) VALUES (2,1,'Abraham','Flores','Razo','2005-06-14','2026-04-14 11:51:12',0);
-INSERT INTO usuario (`id_usuario`,`codigo_tipo_usuario`,`nombre`,`apellido_prim`,`apellido_seg`,`fecha_nacimiento`,`fecha_creacion_usuario`,`estado`) VALUES (3,2,'Brayan','Leon','Frausto','2005-04-19','2026-04-14 11:51:40',0);
+INSERT INTO Usuario (`id_usuario`,`codigo_tipo_usuario`,`nombre`,`apellido_prim`,`apellido_seg`,`fecha_nacimiento`,`fecha_creacion_usuario`,`estado`) VALUES (1,0,'John','Garcia','Viray','2005-01-09','2026-04-13 13:11:41',0);
+INSERT INTO Usuario (`id_usuario`,`codigo_tipo_usuario`,`nombre`,`apellido_prim`,`apellido_seg`,`fecha_nacimiento`,`fecha_creacion_usuario`,`estado`) VALUES (2,1,'Abraham','Flores','Razo','2005-06-14','2026-04-14 11:51:12',0);
+INSERT INTO Usuario (`id_usuario`,`codigo_tipo_usuario`,`nombre`,`apellido_prim`,`apellido_seg`,`fecha_nacimiento`,`fecha_creacion_usuario`,`estado`) VALUES (3,2,'Brayan','Leon','Frausto','2005-04-19','2026-04-14 11:51:40',0);
 
-INSERT INTO us_psswds (`id_usuario`,`psswd`,`ultima_modificacion`) VALUES (1,'0a1e4211dda7f090808df28c3d7cf52663cf6f427939581413aa7c38f574b0a2','2026-04-13 13:13:26');
-INSERT INTO us_psswds (`id_usuario`,`psswd`,`ultima_modificacion`) VALUES (2,'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','2026-04-14 11:52:36');
-INSERT INTO us_psswds (`id_usuario`,`psswd`,`ultima_modificacion`) VALUES (3,'03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4','2026-04-14 11:52:36');
+INSERT INTO UsPsswd (`id_usuario`,`psswd`,`ultima_modificacion`) VALUES (1,'0a1e4211dda7f090808df28c3d7cf52663cf6f427939581413aa7c38f574b0a2','2026-04-13 13:13:26');
+INSERT INTO UsPsswd (`id_usuario`,`psswd`,`ultima_modificacion`) VALUES (2,'a665a45920422f9d417e4867efdc4fb8a04a1f3fff1fa07e998e86f7f7a27ae3','2026-04-14 11:52:36');
+INSERT INTO UsPsswd (`id_usuario`,`psswd`,`ultima_modificacion`) VALUES (3,'03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4','2026-04-14 11:52:36');
 
-INSERT INTO us_datos_sensibles (`id_usuario`,`rfc`,`email`) VALUES (1,'hfki28djfngue','john.garcia.viray@uabc.edu.mx');
-INSERT INTO us_datos_sensibles (`id_usuario`,`rfc`,`email`) VALUES (2,'uwndie082937d','abraham.flores.cabanillas@uabc.edu.mx');
-INSERT INTO us_datos_sensibles (`id_usuario`,`rfc`,`email`) VALUES (3,'djwmei019284h','brayan.leon93@uabc.edu.mx');
+INSERT INTO UsDatosSensible (`id_usuario`,`rfc`,`email`) VALUES (1,'hfki28djfngue','john.garcia.viray@uabc.edu.mx');
+INSERT INTO UsDatosSensible (`id_usuario`,`rfc`,`email`) VALUES (2,'uwndie082937d','abraham.flores.cabanillas@uabc.edu.mx');
+INSERT INTO UsDatosSensible (`id_usuario`,`rfc`,`email`) VALUES (3,'djwmei019284h','brayan.leon93@uabc.edu.mx');
